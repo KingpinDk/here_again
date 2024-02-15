9a3339931d262c805e8508907fccf5c1b16b43eb
KingpinDk <simplykingpin@gmailcom>
2024-02-15 20:35:55 +0530

    on screen buttons added character powerups added

--- lib/app_src/levels.dart
+++ lib/app_src/levels.dart
@@ -1,8 +1,7 @@
 import 'dart:io';
 
+import 'package:here_again/game_src/Components/player.dart';
 import 'package:here_again/game_src/game.dart';
-import 'package:flame/components.dart';
-import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 
 import 'package:flutter/cupertino.dart';
@@ -17,14 +16,15 @@ class Levels extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
+    double height = MediaQuery.of(context).size.height;
+    double width = MediaQuery.of(context).size.width;
 
     return Scaffold(
       body: Center(
         child: IconButton(onPressed: () {
           Navigator.of(context).push(
               MaterialPageRoute(
-                  builder: (context) => GameWidget(game: HereAgain(character: 'Paari', isMobile: (!kIsWeb)&&(Platform.isAndroid || Platform.isIOS), isPc: (kIsWeb) || (Platform.isAndroid || Platform.isIOS)))
+                  builder: (context) => GameWidget(game: HereAgain(player: Player(character: "Malala"), height: height, width: width, character: 'Paari', isMobile: (!kIsWeb)&&(Platform.isAndroid || Platform.isIOS), isPc: (kIsWeb) || (Platform.isAndroid || Platform.isIOS)))
               ));
           },
           icon: Image.asset("assets/images/Menu/Buttons/Play.png", width: 60, height: 60,), )
--- lib/game_src/game.dart
+++ lib/game_src/game.dart
@@ -5,27 +5,38 @@ import 'package:flame/components.dart';
 import 'package:flame/events.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flame/palette.dart';
+import 'package:flame/widgets.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'Components/level.dart';
 import 'Components/player.dart';
 
 
-class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbacks{
+class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbacks, TapCallbacks {
 
   late final CameraComponent cam;
-  late String character;
+  double height;
+  double width;
+  String character;
   bool isMobile;
   bool isPc;
+  bool isJoy;
+  Player player;
+
+  HereAgain(
+      {required this.player,required this.character, required this.isMobile, required this.isPc, this.isJoy = false, required this.height, required this.width});
 
 
-  HereAgain({required this.character, required this.isMobile, required this.isPc});
-
-  Player player = Player(character: "Paari");
-
+  bool isMarked = false;
+  Vector2 markedPos = Vector2(0, 0);
+  bool isFast = false;
   late JoystickComponent joystick;
   late SpriteComponent mask;
-
+  late HudButtonComponent controlBtn;
+  late ToggleButtonComponent toggleButtonComponent;
+  late Vector2 screenSize;
 
   @override
   Color backgroundColor() => const Color(0xFF000000);
@@ -33,14 +44,18 @@ class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbac
 
   @override
   void update(double dt) {
-    if(isMobile) {
-      updateJoystick();
+    if (isMobile) {
+      if (isJoy) {
+        updateJoystick();
+      }
     }
-    mask.position = player.position - Vector2(632,348);
+    mask.position = player.position - Vector2(632, 348);
     super.update(dt);
   }
+
   @override
-  FutureOr<void> onLoad()  async {
+  FutureOr<void> onLoad() async {
+
     //debugMode = true;
     await images.loadAll([
       'Items/Boxes/Box1/idle_perfect.png',
@@ -71,7 +86,8 @@ class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbac
 
 
     final world = Level(levelName: "level01.tmx", player: player);
-    cam = CameraComponent.withFixedResolution(world: world, width: 328, height: 186);
+    cam = CameraComponent.withFixedResolution(
+        world: world, width: 328, height: 186);
     cam.viewfinder.anchor = Anchor.center;
     addAll([cam, world]);
 
@@ -80,14 +96,21 @@ class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbac
 
 
     cam.follow(player);
-
-    if(isMobile) {
-      addJoyStick();
+    addSettings();
+    addMusic();
+    if (isMobile) {
+      if (isJoy) {
+        addJoyStick();
+      }
+      else {
+        addDirController();
+      }
+      addToggleButton();
     }
     return super.onLoad();
   }
 
-  void addJoyStick(){
+  void addJoyStick() {
     joystick = JoystickComponent(
       priority: 45678234567,
       knob: CircleComponent(
@@ -104,8 +127,7 @@ class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbac
   }
 
   void updateJoystick() async {
-    switch(joystick.direction){
-
+    switch (joystick.direction) {
       case JoystickDirection.down:
         Future.delayed(const Duration(milliseconds: 100));
         player.verticalMovement = 1;
@@ -130,9 +152,137 @@ class HereAgain extends FlameGame with HasKeyboardHandlerComponents, DragCallbac
         player.verticalMovement = 0;
         player.horizontalMovement = 0;
         break;
-
     }
   }
 
+  void addToggleButton() async {
 
+    if(player.character == "Paari"){
+      final fastBtnLight = await Sprite.load('Controls/fast_grey.png');
+      final fastBtnDark = await Sprite.load('Controls/fast_white.png');
+      toggleButtonComponent = ToggleButtonComponent(
+        position: Vector2(width-120, height-150),
+        priority: 4567899876523,
+        onPressed: (){
+          if(isFast){
+            isFast = false;
+            player.moveSpeed = 225;
+          }
+          else{
+            isFast = true;
+            player.moveSpeed = 500;
+          }
+        },
+        defaultSelectedSkin:SpriteComponent(
+          sprite: fastBtnDark,
+        ),
+        defaultSkin: SpriteComponent(
+            sprite: fastBtnLight,
+        )
+      );
+    }
+    else{
+      final teleBtnLight = await Sprite.load('Controls/teleport_grey.png');
+      final teleBtnDark = await Sprite.load('Controls/teleport_white.png');
+
+      toggleButtonComponent = ToggleButtonComponent(
+        position: Vector2(width-120, height-150),
+        priority: 4567899876523,
+        onPressed: (){
+
+          if(isMarked){
+            isMarked = false;
+            player.position = markedPos;
+          }
+          else{
+            isMarked = true;
+            markedPos = Vector2.copy(player.position);
+          }
+        },
+        defaultSelectedSkin: SpriteComponent(
+          sprite: teleBtnLight,
+        ),
+        defaultSkin: SpriteComponent(
+            sprite: teleBtnDark,
+        ),
+      );
+
+    }
+
+
+
+    add(toggleButtonComponent);
+  }
+
+  Future<void> addDirController() async {
+
+    final rightBtnSprite = await Sprite.load('Controls/right_white.png');
+    final tapRightBtnSprite = await Sprite.load('Controls/right_grey.png');
+    addButton(rightBtnSprite, tapRightBtnSprite, (){if(player.verticalMovement == 0)player.horizontalMovement = 1;}, 130, 90);
+
+    final leftBtnSprite = await Sprite.load('Controls/left_white.png');
+    final tapLeftBtnSprite = await Sprite.load('Controls/left_grey.png');
+    addButton(leftBtnSprite, tapLeftBtnSprite, (){if(player.verticalMovement == 0)player.horizontalMovement = -1;}, 10, 90);
+
+    final upBtnSprite = await Sprite.load('Controls/up_white.png');
+    final tapUpBtnSprite = await Sprite.load('Controls/up_grey.png');
+    addButton(upBtnSprite, tapUpBtnSprite, (){if(player.horizontalMovement == 0)player.verticalMovement = -1;}, 70, 150);
+
+    final downBtnSprite = await Sprite.load('Controls/down_white.png');
+    final tapDownBtnSprite = await Sprite.load('Controls/down_grey.png');
+    addButton(downBtnSprite, tapDownBtnSprite, (){if(player.horizontalMovement == 0)player.verticalMovement = 1;}, 70, 30);
+
+
+  }
+  void addButton(Sprite idle, Sprite tapped, onTap, double left, double bottom){
+    controlBtn = HudButtonComponent(
+      margin: EdgeInsets.only(left: left, bottom: bottom),
+
+        priority: 4556786789,
+        button: SpriteComponent(
+          size: Vector2.all(65),
+            sprite: idle
+        ),
+        buttonDown: SpriteComponent(
+            sprite: tapped,
+          size: Vector2.all(70),
+        ),
+      onPressed: onTap,
+      onReleased: (){
+        player.horizontalMovement = 0;
+        player.verticalMovement = 0;
+      }
+    );
+    add(controlBtn);
+  }
+
+  void addSettings() async{
+    final settings = await Sprite.load('Controls/settings_white.png');
+    final settingsOnTap = await Sprite.load('Controls/settings_grey.png');
+
+    final settingsHud = HudButtonComponent(
+      priority: 456782345678678,
+      button: SpriteComponent( sprite:  settings, size: Vector2.all(32)),
+      buttonDown: SpriteComponent(sprite: settingsOnTap, size: Vector2.all(34) ),
+      margin: const EdgeInsets.only(top: 21, right: 21),
+
+    );
+    add(settingsHud);
+  }
+
+  void addMusic() async{
+    bool isOn = true;
+    final musicOn = await Sprite.load('Controls/music_on.png');
+    final musicOff = await Sprite.load('Controls/music_off.png');
+
+    final musicToggle = ToggleButtonComponent(
+      priority: 4567890987654,
+      size:Vector2.all(32),
+      defaultSkin: SpriteComponent(sprite: musicOn),
+      defaultSelectedSkin: SpriteComponent(sprite: musicOff),
+      position: Vector2(21, 21),
+    );
+    add(musicToggle);
+  }
 }
+
